b684c335aeffbecb6338ea3a526a2f1ab1f4f0fc
Muskan115315 <107237485+Muskan115315@users.noreply.github.com>
2022-06-17 15:38:54 +0530

    Add files via upload
    boolean expression

---
+++ truefalse.py
@@ -0,0 +1,19 @@
+# day = "Saturday"
+# temperature = 30
+# raining = False
+#
+# if day == "Friday" and temperature > 27 or not raining:
+#     print("Go Swimming")
+# else:
+#     print("Learn Python")
+
+if 0:
+    print("True")
+else:
+    print("False")
+
+name = input("Please enter your name:")
+if name:
+    print("Hello, {}".format(name))
+else:
+    print("Are you the man with no name?")
