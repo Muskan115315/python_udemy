1124df78a4405c21e78147dd98420c6ee4c7a143
Muskan115315 <107237485+Muskan115315@users.noreply.github.com>
2022-06-22 11:24:05 +0530

    Add files via upload

---
+++ summary_challenge.py
@@ -0,0 +1,13 @@
+choice = "-"
+while choice != "0":
+    if choice in "12345":
+        print("You chose {}".format(choice))
+    else:
+        print("Please choose your option from the list below:")
+        print("1:\t Learn Python")
+        print("2:\t Learn Java")
+        print("3:\t Go Swimming")
+        print("4:\t Have dinner")
+        print("5:\t Go to bed")
+        print("0:\t Exit")
+    choice = input()
