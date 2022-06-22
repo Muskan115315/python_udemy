15dc5315de38f38e87037fafc445efdc2b31fb82
Muskan115315 <107237485+Muskan115315@users.noreply.github.com>
2022-06-22 11:02:31 +0530

    Add files via upload

--- guessinggame.py
+++ guessinggame.py
@@ -1,47 +1,25 @@
-answer = 5
+import random
 
-print("Please guess a number between 1 and 10:")
-guess = int(input())
+highest = 100
+answer = random.randint(1, highest)
+print(answer)   # TODO: remove after testing
+guess = 0   # initialise01 to any number that doesn't equal the answer
+print("Please guess a number between 1 and {}:".format(highest))
 
-if guess == answer:
-    print("You got it first time")
-else:
-    if guess < answer:
-        print("Please guess higher")
-    else:   # guess must be greater than answer
-        print("Please guess lower")
+while guess != answer:
     guess = int(input())
+
+    if guess == 0:
+        break
     if guess == answer:
         print("Well done, you guessed it")
     else:
-        print("Sorry, you have not guessed correctly")
-
-# if guess != answer:
-#     if guess < answer:
-#         print("Please guess higher")
-#     else:   # guess must be greater than answer
-#         print("Please guess lower")
-#     guess = int(input())
-#     if guess == answer:
-#         print("Well done, you guessed it")
-#     else:
-#         print("Sorry, you have not guessed correctly")
+        if guess < answer:
+            print("Please guess higher")
+        else:   # guess must be greater than answer
+            print("Please guess lower")
+# guess = int(input())
+# if guess == answer:
+#     print("Well done, you guessed it")
 # else:
-#     print("You got it first time")
-
-# if guess < answer:
-#     print("Please guess higher")
-#     guess = int(input())
-#     if guess == answer:
-#         print("Well done, you  guessed it")
-#     else:
-#         print("Sorry you have not guessed correctly")
-# elif guess > answer:
-#     print("Please guess lower")
-#     guess = int(input())
-#     if guess == answer:
-#         print("Well done, you  guessed it")
-#     else:
-#         print("Sorry you have not guessed correctly")
-# else:
-#     print("You got it first time")
+#     print("Sorry, you have not guessed correctly")
